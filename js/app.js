35b8c8db92ec6e30160c20d5b3ff75d0149c4ebe
ROMAN <poman207575@gmail.com>
2023-01-24 19:16:00 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 
 	const showSlider = new Swiper('.showcase-carousel', {
-		loop: true,
+		// loop: true,
 		slidesPerView: 3,
-		speed: 1800,
+		speed: 1200,
 		centeredSlides: true,
+        mousewheel: true,
+		autoplay: true,
+		navigation: {
+			nextEl: '.showcase-navigation__next',
+			prevEl: '.showcase-navigation__prev',
+		},
     });
 
      document.querySelector('video').playbackRate = 2;
