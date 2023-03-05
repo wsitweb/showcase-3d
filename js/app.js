b384c29f99ba23b20458a4227c727a34b8c6131e
ROMAN <poman207575@gmail.com>
2023-03-05 15:14:11 +0200

    dev

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i
+    .test(navigator.userAgent)){
+		slidesPerView = 1;
+	}else{
+		slidesPerView = 3;
+	};
 
 	const showSlider = new Swiper('.showcase-carousel', {
-		// loop: true,
-		slidesPerView: 3,
+		loop: true,
+		slidesPerView: slidesPerView,
 		speed: 1200,
 		centeredSlides: true,
         mousewheel: true,
-		autoplay: true,
+		// autoplay: true,
 		navigation: {
 			nextEl: '.showcase-navigation__next',
 			prevEl: '.showcase-navigation__prev',
 		},
     });
 
-     document.querySelector('video').playbackRate = 2;
+    //  document.querySelector('video').playbackRate = 2;
 
  });
